1e79fe44e83a8996db3f38e85b5689f63d8b2f9e
rino <rino@ainosi.co.id>
2019-07-08 14:17:27 +0700

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div>
+    <!-- Navigation -->
+    <Navigation/>
+    <!-- Content -->
+    <div class="container content">
+      <!-- Content -->
+      <Banner />
+      <nuxt/>
+    </div>
+    <!-- Footer -->
+    <div class="footer">
+      Copyright here
+    </div>
+  </div>
+</template>
+
+<script>
+import Navigation from '~/components/navigation.vue'
+import Banner from '~/components/banner.vue'
+export default {
+  components: {
+    Navigation,
+    Banner
+  }
+}
+</script>
